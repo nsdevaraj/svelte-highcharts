d895a0a14d638afc91d5a69fca0167e507660463
N.S.Devaraj <nsdevaraj@gmail.com>
2019-12-30 19:02:35 +0530

    css fixes

--- src/App.svelte
+++ src/App.svelte
@@ -1,15 +1,14 @@
 <script>
   import { afterUpdate } from "svelte";
-  import Highcharts from "highcharts";
-  import data from "highcharts/modules/data";
-  let canvas;
-  export let highchartstype;
-  export let chartjson;
-  data(Highcharts);
-  afterUpdate(() => {
+  import Highcharts from "highcharts"; 
+  let canvas; 
+  export let chartjson; 
+  export let highchartboxwidth;
+  export let highchartboxheight; 
+  afterUpdate(() => { 
     setTimeout(() => {
       Highcharts.chart(canvas, chartjson);
-    }, 0);
+    }, 10);
   });
 </script>
 
@@ -17,22 +16,23 @@
   /* 
   Setting custom css variables enables the user to use css to target a custom
   element by an attribute and change css properties that you want to expose.
-  */
-  div {
+  */ 
+ div {
     display: flex;
     align-items: center;
-    justify-content: space-between;
-    height: 100%;
-    width: 100%;
+    height:  var(--chart-height, auto);
+    width: var(--chart-width, auto);
   }
-</style>
+  </style>
 
-<div role="chart" bind:this={canvas}>
+<div bind:this={canvas} style="--chart-height: {highchartboxheight}; --chart-width: {highchartboxwidth}">
   <slot />
 </div>
-<svelte:options tag="highchart-box" />
+<svelte:options tag="highchart-box"/>
 <!-- 
-
+ For old Firefox enable these twofire 
+  dom.webcomponents.enabled
+  dom.webcomponents.customelements.enabled
 This tells the Svelte compiler that this file is a custom element. 
 We also have to include the "customElement: true" compiler setting in rollup configuration.
 -->
