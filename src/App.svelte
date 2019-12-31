08f5b406f85b33e7f81eb1bfe6299202bf4eaaa1
N.S.Devaraj <nsdevaraj@gmail.com>
2019-12-31 11:22:02 +0530

    extra modules added

--- src/App.svelte
+++ src/App.svelte
@@ -1,16 +1,26 @@
 <script>
   import { afterUpdate } from "svelte";
-  import Highcharts from "highcharts";  
+  import Highcharts from "highcharts";
+  import highchartsMore from "highcharts-more";
   import funnel from "highcharts/modules/funnel";
-   
+  import data from "highcharts/modules/data";
+  import exporting from "highcharts/modules/exporting";
+
+  highchartsMore(Highcharts);
+  data(Highcharts);
+  exporting(Highcharts);
   funnel(Highcharts);
-  let canvas; 
-  export let chartjson; 
+
+  let canvas;
+
+  export let chartjson;
   export let highchartboxwidth;
-  export let highchartboxheight; 
-  afterUpdate(() => { 
-    setTimeout(() => {
-      Highcharts.chart(canvas, chartjson);
+  export let highchartboxheight;
+  export let dataJSON;
+
+  afterUpdate(() => {
+    setTimeout(() => { 
+        Highcharts.chart(canvas, chartjson); 
     }, 10);
   });
 </script>
@@ -19,19 +29,21 @@
   /* 
   Setting custom css variables enables the user to use css to target a custom
   element by an attribute and change css properties that you want to expose.
-  */ 
- div {
+  */
+  div {
     display: flex;
     align-items: center;
-    height:  var(--chart-height, auto);
+    height: var(--chart-height, auto);
     width: var(--chart-width, auto);
   }
-  </style>
+</style>
 
-<div bind:this={canvas} style="--chart-height: {highchartboxheight}; --chart-width: {highchartboxwidth}">
+<div
+  bind:this={canvas}
+  style="--chart-height: {highchartboxheight}; --chart-width: {highchartboxwidth}">
   <slot />
 </div>
-<svelte:options tag="highchart-box"/>
+<svelte:options tag="highchart-box" />
 <!-- 
  For old Firefox enable these twofire 
   dom.webcomponents.enabled
